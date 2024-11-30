ed13143316b36ba93f344b3e772bdbeeeded72c1
Pablo Galve <107808655+PablGalveGR@users.noreply.github.com>
2024-11-30 21:14:02 +0100

    Basic routing implemented no Detail view routing

--- frontEnd/tour-of-heroes/src/app/app.component.ts
+++ frontEnd/tour-of-heroes/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroesComponent, MessagesComponent],
+  imports: [ HeroesComponent, MessagesComponent, AppRoutingModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
